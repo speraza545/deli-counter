deb890fd743e8cf281cf1eea8700168a6f468d4a
speraza545 <s.peraza545@gmail.com>
2021-02-23 15:53:28 -0500

    commit tree 57821a3cc557d68de8f767a5c5ae7c0b255200a9

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(line)
+    if line.size == 0
+        puts "The line is currently empty."
+    else
+        place = 1
+        new_arr = ["The line is currently:"]
+        line.each do |person|
+            new_arr.push("#{place}. #{person}")
+            place += 1
+        end
+        puts new_arr.join(" ")
+    end
+end
+
+def take_a_number(current_queue, add_to_queue)
+        current_queue.push(add_to_queue)
+        puts "Welcome, #{add_to_queue}. You are number #{current_queue.size} in line."
+end
+
+def now_serving(arr)
+    if arr.size > 0
+        puts "Currently serving #{arr[0]}."
+        arr.shift
+    else
+    puts "There is nobody waiting to be served!"
+    end
+end
+
+now_serving(["logan", "avi", "jack"])
